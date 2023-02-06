bc0eb715d9125fc2793b309dda5e779402b43c2a
MegaChuck64 <cjscottcarey@yahoo.com>
2023-02-05 23:59:47 -0500

    testing different escape sequences, added PrintF function

--- VTS_LIB/Terminal.cs
+++ VTS_LIB/Terminal.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Win32.SafeHandles;
-using System.Reflection.PortableExecutable;
 using System.Runtime.InteropServices;
 
 namespace VTS_LIB;
@@ -81,29 +80,31 @@ public static class Terminal
             }
         }
 
+        //https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences
         if (!h.IsInvalid)
         {
-            for (int i = 0; i < 10; i++)
-            {
-                var line = string.Empty;
-
-                for (byte character = 65; character < 65 + 26; ++character)
-                {
-                    line += (char)character;
-                }
-
-                bool goodWrite = WriteConsole(hOut, "\x1b[31m" + line, (uint)line.Length, out uint cchwritten, 0);
-                
-                if (!goodWrite)
-                {
-                    var lastError = Marshal.GetLastWin32Error();
-                    System.Diagnostics.Debug.WriteLine($"Error drawing fast console output: \t{lastError}");
-                }
-
-                Console.ReadKey();                
-            }
+            PrintF(hOut, "\x1b[31mThis text has a red foreground using SGR.31.\r\n");
+            PrintF(hOut, "\x1b[1mThis text has a bright (bold) red foreground using SGR.1 to affect the previous color setting.\r\n");
+            PrintF(hOut, "\x1b[mThis text has returned to default colors using SGR.0 implicitly.\r\n");
+            PrintF(hOut, "\x1b[34;46mThis text shows the foreground and background change at the same time.\r\n");
+            PrintF(hOut, "\x1b[0mThis text has returned to default colors using SGR.0 explicitly.\r\n");
+            PrintF(hOut, "\x1b[31;32;33;34;35;36;101;102;103;104;105;106;107mThis text attempts to apply many colors in the same command. Note the colors are applied from left to right so only the right-most option of foreground cyan (SGR.36) and background bright white (SGR.107) is effective.\r\n");
+            PrintF(hOut, "\x1b[39mThis text has restored the foreground color only.\r\n");
+            PrintF(hOut, "\x1b[49mThis text has restored the background color only.\r\n");
         }
-        Console.ReadKey();
+
     }
 
+
+    public static void PrintF(IntPtr hOut, string msg)
+    {
+        bool goodWrite = WriteConsole(hOut, msg, (uint)msg.Length, out uint cchwritten, 0);
+
+        if (!goodWrite)
+        {
+            var lastError = Marshal.GetLastWin32Error();
+            System.Diagnostics.Debug.WriteLine($"Error drawing fast console output: \t{lastError}");
+        }
+
+    }
 }
